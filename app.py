cccd8946d6a81a5734f23f38c735ceb24a29b1cc
Pulkit Sharma <pulkitsharma151206@gmail.com>
2026-05-11 13:02:16 +0530

    2

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pickle
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+
 
 # --- 1. PERFORMANCE METRICS (From Notebook) ---
 model_performance = {
